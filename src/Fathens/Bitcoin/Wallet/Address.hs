5a94709f7371251b6999548d983558c87cb19c1d
Kunio Sawatani <devel@fathens.org>
2017-07-30 10:33:43 +0900

    Fomat AddressPrefix

--- src/Fathens/Bitcoin/Wallet/Address.hs
+++ src/Fathens/Bitcoin/Wallet/Address.hs
@@ -19,78 +19,91 @@ import qualified Data.Text.Lazy       as T
 
 -- Data
 
-data AddressPrefix = AddressPrefix { isTest :: Bool
-                   , symbols                :: [String]
-                   , prefix                 :: ByteString
-                   , suffix                 :: ByteString
-                   } deriving (Show, Eq)
+data AddressPrefix = AddressPrefix {
+  isTest  :: Bool
+, symbols :: [String]
+, prefix  :: ByteString
+, suffix  :: ByteString
+} deriving (Show, Eq)
 
 prefixP2PKH :: Bool -> AddressPrefix
-prefixP2PKH False = AddressPrefix { isTest = False
-                            , symbols = ["1"]
-                            , prefix = BS.singleton 0x00
-                            , suffix = BS.empty
-                            }
-prefixP2PKH True = AddressPrefix { isTest = True
-                           , symbols = ["m", "n"]
-                           , prefix = BS.singleton 0x6f
-                           , suffix = BS.empty
-                           }
+prefixP2PKH False = AddressPrefix {
+  isTest = False
+, symbols = ["1"]
+, prefix = BS.singleton 0x00
+, suffix = BS.empty
+}
+prefixP2PKH True = AddressPrefix {
+  isTest = True
+, symbols = ["m", "n"]
+, prefix = BS.singleton 0x6f
+, suffix = BS.empty
+}
 prefixP2SH :: Bool -> AddressPrefix
-prefixP2SH False = AddressPrefix { isTest = False
-                           , symbols = ["3"]
-                           , prefix = BS.singleton 0x05
-                           , suffix = BS.empty
-                           }
-prefixP2SH True = AddressPrefix { isTest = True
-                          , symbols = ["2"]
-                          , prefix = BS.singleton 0xc4
-                          , suffix = BS.empty
-                          }
+prefixP2SH False = AddressPrefix {
+  isTest = False
+, symbols = ["3"]
+, prefix = BS.singleton 0x05
+, suffix = BS.empty
+}
+prefixP2SH True = AddressPrefix {
+  isTest = True
+, symbols = ["2"]
+, prefix = BS.singleton 0xc4
+, suffix = BS.empty
+}
 prefixPRV :: Bool -> AddressPrefix
-prefixPRV False = AddressPrefix { isTest = False
-                          , symbols = ["5"]
-                          , prefix = BS.singleton 0x80
-                          , suffix = BS.empty
-                          }
-prefixPRV True = AddressPrefix { isTest = True
-                         , symbols = ["9"]
-                         , prefix = BS.singleton 0xef
-                         , suffix = BS.empty
-                         }
+prefixPRV False = AddressPrefix {
+  isTest = False
+, symbols = ["5"]
+, prefix = BS.singleton 0x80
+, suffix = BS.empty
+}
+prefixPRV True = AddressPrefix {
+  isTest = True
+, symbols = ["9"]
+, prefix = BS.singleton 0xef
+, suffix = BS.empty
+}
 prefixCPRV :: Bool -> AddressPrefix
-prefixCPRV False = AddressPrefix { isTest = False
-                           , symbols = ["K", "L"]
-                           , prefix = BS.singleton 0x80
-                           , suffix = BS.singleton 0x01
-                           }
-prefixCPRV True = AddressPrefix { isTest = True
-                          , symbols = ["c"]
-                          , prefix = BS.singleton 0xef
-                          , suffix = BS.singleton 0x01
-                          }
+prefixCPRV False = AddressPrefix {
+  isTest = False
+, symbols = ["K", "L"]
+, prefix = BS.singleton 0x80
+, suffix = BS.singleton 0x01
+}
+prefixCPRV True = AddressPrefix {
+  isTest = True
+, symbols = ["c"]
+, prefix = BS.singleton 0xef
+, suffix = BS.singleton 0x01
+}
 prefixXPUB :: Bool -> AddressPrefix
-prefixXPUB False = AddressPrefix { isTest = False
-                           , symbols = ["xpub"]
-                           , prefix = BS.pack [0x04, 0x88, 0xb2, 0x1e]
-                           , suffix = BS.empty
-                           }
-prefixXPUB True = AddressPrefix { isTest = True
-                          , symbols = ["tpub"]
-                          , prefix = BS.pack [0x04, 0x35, 0x87, 0xcf]
-                          , suffix = BS.empty
-                          }
+prefixXPUB False = AddressPrefix {
+  isTest = False
+, symbols = ["xpub"]
+, prefix = BS.pack [0x04, 0x88, 0xb2, 0x1e]
+, suffix = BS.empty
+}
+prefixXPUB True = AddressPrefix {
+  isTest = True
+, symbols = ["tpub"]
+, prefix = BS.pack [0x04, 0x35, 0x87, 0xcf]
+, suffix = BS.empty
+}
 prefixXPRV :: Bool -> AddressPrefix
-prefixXPRV False = AddressPrefix { isTest = False
-                           , symbols = ["xprv"]
-                           , prefix = BS.pack [0x04, 0x88, 0xad, 0xe4]
-                           , suffix = BS.empty
-                           }
-prefixXPRV True = AddressPrefix { isTest = True
-                          , symbols = ["tprv"]
-                          , prefix = BS.pack [0x04, 0x35, 0x83, 0x94]
-                          , suffix = BS.empty
-                          }
+prefixXPRV False = AddressPrefix {
+  isTest = False
+, symbols = ["xprv"]
+, prefix = BS.pack [0x04, 0x88, 0xad, 0xe4]
+, suffix = BS.empty
+}
+prefixXPRV True = AddressPrefix {
+  isTest = True
+, symbols = ["tprv"]
+, prefix = BS.pack [0x04, 0x35, 0x83, 0x94]
+, suffix = BS.empty
+}
 
 -- Functions
 
