defa30740f4cbe7474e5a13fdd94ac487cc90c44
Kunio Sawatani <devel@fathens.org>
2017-07-30 19:05:22 +0900

    Add isCompressed to AddressPrefix

--- src/Fathens/Bitcoin/Wallet/Address.hs
+++ src/Fathens/Bitcoin/Wallet/Address.hs
@@ -1,5 +1,5 @@
 module Fathens.Bitcoin.Wallet.Address (
-  AddressPrefix
+  AddressPrefix(isTestnet)
 , prefixP2PKH
 , prefixP2SH
 , prefixPRV
@@ -9,6 +9,7 @@ module Fathens.Bitcoin.Wallet.Address (
 , findBySymbol
 , getPayload
 , appendPayload
+, isCompressed
 ) where
 
 import           Control.Monad
@@ -21,23 +22,23 @@ import qualified Data.Text.Lazy       as T
 -- Data
 
 data AddressPrefix = AddressPrefix {
-  isTest  :: Bool
-, symbols :: [String]
-, prefix  :: ByteString
-, suffix  :: ByteString
+  isTestnet :: Bool
+, symbols   :: [String]
+, prefix    :: ByteString
+, suffix    :: ByteString
 } deriving (Show, Eq)
 
 prefixP2PKH :: Bool -> AddressPrefix
 prefixP2PKH False = singleton_P2PKH_False
 prefixP2PKH True  = singleton_P2PKH_True
 singleton_P2PKH_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["1"]
 , prefix = BS.singleton 0x00
 , suffix = BS.empty
 }
 singleton_P2PKH_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["m", "n"]
 , prefix = BS.singleton 0x6f
 , suffix = BS.empty
@@ -46,13 +47,13 @@ prefixP2SH :: Bool -> AddressPrefix
 prefixP2SH False = singleton_P2SH_False
 prefixP2SH True  = singleton_P2SH_True
 singleton_P2SH_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["3"]
 , prefix = BS.singleton 0x05
 , suffix = BS.empty
 }
 singleton_P2SH_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["2"]
 , prefix = BS.singleton 0xc4
 , suffix = BS.empty
@@ -61,13 +62,13 @@ prefixPRV :: Bool -> AddressPrefix
 prefixPRV False = singleton_PRV_False
 prefixPRV True  = singleton_PRV_True
 singleton_PRV_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["5"]
 , prefix = BS.singleton 0x80
 , suffix = BS.empty
 }
 singleton_PRV_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["9"]
 , prefix = BS.singleton 0xef
 , suffix = BS.empty
@@ -76,13 +77,13 @@ prefixCPRV :: Bool -> AddressPrefix
 prefixCPRV False = singleton_CPRV_False
 prefixCPRV True  = singleton_CPRV_True
 singleton_CPRV_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["K", "L"]
 , prefix = BS.singleton 0x80
 , suffix = BS.singleton 0x01
 }
 singleton_CPRV_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["c"]
 , prefix = BS.singleton 0xef
 , suffix = BS.singleton 0x01
@@ -91,13 +92,13 @@ prefixXPUB :: Bool -> AddressPrefix
 prefixXPUB False = singleton_XPUB_False
 prefixXPUB True  = singleton_XPUB_True
 singleton_XPUB_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["xpub"]
 , prefix = BS.pack [0x04, 0x88, 0xb2, 0x1e]
 , suffix = BS.empty
 }
 singleton_XPUB_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["tpub"]
 , prefix = BS.pack [0x04, 0x35, 0x87, 0xcf]
 , suffix = BS.empty
@@ -106,17 +107,19 @@ prefixXPRV :: Bool -> AddressPrefix
 prefixXPRV False = singleton_XPRV_False
 prefixXPRV True  = singleton_XPRV_True
 singleton_XPRV_False = AddressPrefix {
-  isTest = False
+  isTestnet = False
 , symbols = ["xprv"]
 , prefix = BS.pack [0x04, 0x88, 0xad, 0xe4]
 , suffix = BS.empty
 }
 singleton_XPRV_True = AddressPrefix {
-  isTest = True
+  isTestnet = True
 , symbols = ["tprv"]
 , prefix = BS.pack [0x04, 0x35, 0x83, 0x94]
 , suffix = BS.empty
 }
+
+-- Classes
 
 -- Functions
 
@@ -136,6 +139,13 @@ getPayload p src = do
 
 appendPayload :: AddressPrefix -> ByteString -> ByteString
 appendPayload p src = BS.concat [prefix p, src, suffix p]
+
+isCompressed :: AddressPrefix -> Bool -- TODO classify
+isCompressed a | a == singleton_CPRV_False = True
+               | a == singleton_CPRV_True  = True
+               | a == singleton_PRV_False  = False
+               | a == singleton_PRV_True   = False
+               | otherwise                = undefined
 
 -- Utilities
 
