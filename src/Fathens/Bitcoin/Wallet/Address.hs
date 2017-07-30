6380b874bb8be6abce491a9d2e813df1dc724b17
Kunio Sawatani <devel@fathens.org>
2017-07-30 13:07:04 +0900

    Use singleton for AddressPrefix

--- src/Fathens/Bitcoin/Wallet/Address.hs
+++ src/Fathens/Bitcoin/Wallet/Address.hs
@@ -28,78 +28,90 @@ data AddressPrefix = AddressPrefix {
 } deriving (Show, Eq)
 
 prefixP2PKH :: Bool -> AddressPrefix
-prefixP2PKH False = AddressPrefix {
+prefixP2PKH False = singleton_P2PKH_False
+prefixP2PKH True  = singleton_P2PKH_True
+singleton_P2PKH_False = AddressPrefix {
   isTest = False
 , symbols = ["1"]
 , prefix = BS.singleton 0x00
 , suffix = BS.empty
 }
-prefixP2PKH True = AddressPrefix {
+singleton_P2PKH_True = AddressPrefix {
   isTest = True
 , symbols = ["m", "n"]
 , prefix = BS.singleton 0x6f
 , suffix = BS.empty
 }
 prefixP2SH :: Bool -> AddressPrefix
-prefixP2SH False = AddressPrefix {
+prefixP2SH False = singleton_P2SH_False
+prefixP2SH True  = singleton_P2SH_True
+singleton_P2SH_False = AddressPrefix {
   isTest = False
 , symbols = ["3"]
 , prefix = BS.singleton 0x05
 , suffix = BS.empty
 }
-prefixP2SH True = AddressPrefix {
+singleton_P2SH_True = AddressPrefix {
   isTest = True
 , symbols = ["2"]
 , prefix = BS.singleton 0xc4
 , suffix = BS.empty
 }
 prefixPRV :: Bool -> AddressPrefix
-prefixPRV False = AddressPrefix {
+prefixPRV False = singleton_PRV_False
+prefixPRV True  = singleton_PRV_True
+singleton_PRV_False = AddressPrefix {
   isTest = False
 , symbols = ["5"]
 , prefix = BS.singleton 0x80
 , suffix = BS.empty
 }
-prefixPRV True = AddressPrefix {
+singleton_PRV_True = AddressPrefix {
   isTest = True
 , symbols = ["9"]
 , prefix = BS.singleton 0xef
 , suffix = BS.empty
 }
 prefixCPRV :: Bool -> AddressPrefix
-prefixCPRV False = AddressPrefix {
+prefixCPRV False = singleton_CPRV_False
+prefixCPRV True  = singleton_CPRV_True
+singleton_CPRV_False = AddressPrefix {
   isTest = False
 , symbols = ["K", "L"]
 , prefix = BS.singleton 0x80
 , suffix = BS.singleton 0x01
 }
-prefixCPRV True = AddressPrefix {
+singleton_CPRV_True = AddressPrefix {
   isTest = True
 , symbols = ["c"]
 , prefix = BS.singleton 0xef
 , suffix = BS.singleton 0x01
 }
 prefixXPUB :: Bool -> AddressPrefix
-prefixXPUB False = AddressPrefix {
+prefixXPUB False = singleton_XPUB_False
+prefixXPUB True  = singleton_XPUB_True
+singleton_XPUB_False = AddressPrefix {
   isTest = False
 , symbols = ["xpub"]
 , prefix = BS.pack [0x04, 0x88, 0xb2, 0x1e]
 , suffix = BS.empty
 }
-prefixXPUB True = AddressPrefix {
+singleton_XPUB_True = AddressPrefix {
   isTest = True
 , symbols = ["tpub"]
 , prefix = BS.pack [0x04, 0x35, 0x87, 0xcf]
 , suffix = BS.empty
 }
 prefixXPRV :: Bool -> AddressPrefix
-prefixXPRV False = AddressPrefix {
+prefixXPRV False = singleton_XPRV_False
+prefixXPRV True  = singleton_XPRV_True
+singleton_XPRV_False = AddressPrefix {
   isTest = False
 , symbols = ["xprv"]
 , prefix = BS.pack [0x04, 0x88, 0xad, 0xe4]
 , suffix = BS.empty
 }
-prefixXPRV True = AddressPrefix {
+singleton_XPRV_True = AddressPrefix {
   isTest = True
 , symbols = ["tprv"]
 , prefix = BS.pack [0x04, 0x35, 0x83, 0x94]
